15c4443e3fdf591af72d7d5fa3cf839d860ca82e
SDN33 <s.deinegri2@gmail.com>
2024-08-28 13:33:56 +0200

    stillinov

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,18 +1,18 @@
-export const metadata = {
-  title: 'StillInov | Agence de communication digitale',
-  description: 'Donnez vie à vos ambitions digitales et transformez vos idées en réalité.',
-}
-
+import Head from 'next/head'
 import Hero from '@/components/hero'
 import Features from '@/components/features'
 import Newsletter from '@/components/newsletter'
 import Testimonials from '@/components/testimonials'
 import AboutUs from '@/components/quisommesnous'
-import  CookieConsentBanner from '@/components/cookies'
+import CookieConsentBanner from '@/components/cookies'
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>StillInov | Agence de communication digitale</title>
+        <meta name="description" content="Donnez vie à vos ambitions digitales et transformez vos idées en réalité." />
+      </Head>
       <Hero />
       <Features />
       <AboutUs />
