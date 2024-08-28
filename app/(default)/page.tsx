7b7108c2688ed0eebd8f413aca378855e014187a
SDN33 <s.deinegri2@gmail.com>
2024-08-28 20:56:42 +0200

    stillinov

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,7 +1,29 @@
 export const metadata = {
-  title: 'StillInov | Agence de communication digitale',
-  description: 'Donnez vie à vos ambitions digitales et transformez vos idées en réalité.',
-}
+  title: 'StillInov | Agence de Communication Digitale - Création, Design & Développement Web',
+  description: 'StillInov, votre partenaire en communication digitale, offre des services personnalisés de création, design, et développement web pour transformer vos idées en réalité, quel que soit votre budget.',
+  keywords: 'communication digitale, création site web, design graphique, développement web, SEO, marketing digital, agence web, solutions personnalisées, budget adaptable',
+  openGraph: {
+    title: 'StillInov | Agence de Communication Digitale',
+    description: 'Découvrez comment StillInov transforme vos idées en projets digitaux réussis grâce à des solutions personnalisées adaptées à vos besoins et à votre budget.',
+    url: 'https://www.stillinov.com',
+    type: 'website',
+    images: [
+      {
+        url: 'https://www.stillinov.com/images/og-image.jpg', // Assurez-vous d'avoir une image dédiée pour Open Graph
+        width: 1200,
+        height: 630,
+        alt: 'StillInov - Agence de Communication Digitale',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@StillInov',
+    title: 'StillInov | Agence de Communication Digitale - Création, Design & Développement Web',
+    description: 'StillInov, votre partenaire en communication digitale, offre des services personnalisés de création, design, et développement web pour transformer vos idées en réalité.',
+  },
+};
+
 
 import Hero from '@/components/hero'
 import Features from '@/components/features'
