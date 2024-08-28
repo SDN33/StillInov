f2cc2d7cbfda66c163fe57a04ba3f38aa6802ca1
SDN33 <s.deinegri2@gmail.com>
2024-08-28 11:57:54 +0200

    stillinov

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -30,7 +30,7 @@ export default function Testimonials() {
               </div>
               <blockquote className="text-lg text-gray-400 grow">— StillInov a créé un site e-commerce pour ma marque de vêtements, avec un design au goût du jour et une partie admin pour un suivi complet de mon activité. Je ne pouvais pas espérer mieux ! </blockquote>
               <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
-                <cite className="text-gray-200 not-italic">Junior Kitsadi</cite> - <a className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">SevenMates</a>
+                <cite className="text-gray-200 not-italic">J.F Kitsadi</cite> - <a className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">SevenMates</a>
               </div>
             </div>
 
