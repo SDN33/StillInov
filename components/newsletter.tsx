f7cf4cc5703a12fabbe9c44232e3c6cc1e6181be
SDN33 <s.deinegri2@gmail.com>
2024-08-28 14:14:45 +0200

    stillinov

--- components/newsletter.tsx
+++ components/newsletter.tsx
@@ -28,7 +28,7 @@ export default function Newsletter() {
             </div>
 
             {/* CTA form */}
-            <form className="w-full lg:w-1/2">
+            <form className="w-full lg:w-1/2" action="https://formspree.io/f/xblrwadg" method="POST">
               <div className="flex flex-col sm:flex-row justify-center max-w-xs mx-auto sm:max-w-md lg:max-w-none">
                 <input type="email" className="w-full appearance-none bg-purple-700 border border-purple-500 focus:border-purple-300 rounded-sm px-4 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-purple-400" placeholder="Votre email…" aria-label="Votre email…" />
                 <a className="btn text-purple-600 bg-purple-100 hover:bg-white shadow" href="#0">S'abonner</a>
