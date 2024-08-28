a56dcd14498b1796e75d7fb8bd446b0f79d1bab0
SDN33 <s.deinegri2@gmail.com>
2024-08-28 13:41:15 +0200

    stillinov

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -8,6 +8,7 @@ import Features from '@/components/features'
 import Newsletter from '@/components/newsletter'
 import Testimonials from '@/components/testimonials'
 import AboutUs from '@/components/quisommesnous'
+import Cookies from '@/components/cookies'
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
       <AboutUs />
       <Testimonials />
       <Newsletter />
-    </>
-  )
+      <Cookies />
+
+    </>)
 }
