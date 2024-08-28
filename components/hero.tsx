655359bb07cfd03a7a53f41c8fc85344a442d831
SDN33 <s.deinegri2@gmail.com>
2024-08-28 20:42:25 +0200

    stillinov

--- components/hero.tsx
+++ components/hero.tsx
@@ -28,7 +28,7 @@ export default function Hero() {
             <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Donnez vie à vos ambitions digitales et transformez vos idées en réalité.</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
-                <a className="btn text-white bg-green-500 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Contactez-nous</a>
+                <a className="btn text-white bg-green-500 hover:bg-green-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Contactez-nous</a>
               </div>
               <div data-aos="fade-up" data-aos-delay="600">
                 <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Voir plus</a>
