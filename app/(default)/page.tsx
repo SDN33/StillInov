82e8415672fb633b94e0342fe30b4f4e7112e590
SDN33 <s.deinegri2@gmail.com>
2024-08-28 13:26:27 +0200

    stillinov

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -9,7 +9,6 @@ import Newsletter from '@/components/newsletter'
 import Testimonials from '@/components/testimonials'
 import AboutUs from '@/components/quisommesnous'
 import Cookies from '@/components/cookies'
-import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
