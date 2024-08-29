e073135d3934b7094648bfb5fd5fc48c73281e63
SDN33 <s.deinegri2@gmail.com>
2024-08-29 19:16:09 +0200

    stillinov

--- components/hero.tsx
+++ components/hero.tsx
@@ -38,14 +38,12 @@ export default function Hero() {
 
           {/* Modal video */}
           <ModalVideo
-            thumb={VideoThumb}
             thumbWidth={1024}
             thumbHeight={576}
             thumbAlt="Modal video thumbnail"
             video="/videos/video.mp4"
             videoWidth={1920}
             videoHeight={1080} />
-
         </div>
 
       </div>
