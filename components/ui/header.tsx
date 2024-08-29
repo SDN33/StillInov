55bb431bd4c909f420f93fcdad8295bb690e941a
SDN33 <s.deinegri2@gmail.com>
2024-08-29 20:27:22 +0200

    stillinov

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -26,10 +26,10 @@ export default function Header() {
           <nav className="hidden md:flex md:grow">
             <ul className="flex grow justify-end flex-wrap items-center">
               <li>
-                <Link href="page#features" className="font-medium text-white hover:text-gray-300 px-5 py-3 flex items-center transition duration-150 ease-in-out">Services</Link>
+                <Link href="/home#features" className="font-medium text-white hover:text-gray-300 px-5 py-3 flex items-center transition duration-150 ease-in-out">Services</Link>
               </li>
               <li>
-                <Link href="page#about" className="font-medium text-white hover:text-gray-300 px-5 py-3 flex items-center transition duration-150 ease-in-out">Qui sommes-nous ?</Link>
+                <Link href="/home#about" className="font-medium text-white hover:text-gray-300 px-5 py-3 flex items-center transition duration-150 ease-in-out">Qui sommes-nous ?</Link>
               </li>
               <li>
                 <Link href="/contact" className="font-medium text-white hover:text-gray-300 px-5 py-3 flex items-center transition duration-150 ease-in-out">Contact</Link>
