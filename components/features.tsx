fac8f0c3cc88ff80242a389aca5eacba4d95adab
SDN33 <s.deinegri2@gmail.com>
2024-08-30 13:33:22 +0200

    stillinov

--- components/features.tsx
+++ components/features.tsx
@@ -1,6 +1,3 @@
-import Image from 'next/image'
-import idVisuelle from '/public/images/Eye3.svg';
-
 export default function Features() {
   return (
     <section>
@@ -19,11 +16,13 @@ export default function Features() {
 
             {/* 1st item */}
             <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
-              <div className="w-16 h-16 mb-4">
-                <Image src={idVisuelle} alt="Identité Visuelle" width={64} height={64} />
-              </div>
-              <h4 className="h4 mb-2">Identité Visuelle</h4>
-              <p className="text-lg text-gray-400 text-center">Création d'une image de marque unique et cohérente pour renforcer votre reconnaissance et impact sur tous supports.</p>
+              <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+                <rect className="fill-current text-purple-600" width="64" height="64" rx="32" />
+                <path className="stroke-current text-purple-100" d="M30 39.313l-4.18 2.197L27 34.628l-5-4.874 6.91-1.004L32 22.49l3.09 6.26L42 29.754l-3 2.924" strokeLinecap="square" strokeWidth="2" fill="none" fillRule="evenodd" />
+                <path className="stroke-current text-purple-300" d="M43 42h-9M43 37h-9" strokeLinecap="square" strokeWidth="2" />
+              </svg>
+              <h4 className="h4 mb-2">Identitée Visuelle</h4>
+              <p className="text-lg text-gray-400 text-center"> Création d'une image de marque unique et cohérente pour renforcer votre reconnaissance et impact sur tous supports.</p>
             </div>
 
             {/* 2nd item */}
@@ -48,7 +47,7 @@ export default function Features() {
                 </g>
               </svg>
               <h4 className="h4 mb-2">Référencement SEO</h4>
-              <p className="text-lg text-gray-400 text-center">Optimiser votre visibilité en ligne pour vous positionner en tête des résultats de recherche et attirer plus de trafic qualifié.</p>
+              <p className="text-lg text-gray-400 text-center"> Optimiser votre visibilité en ligne pour vous positionner en tête des résultats de recherche et attirer plus de trafic qualifié.</p>
             </div>
 
             {/* 4th item */}
