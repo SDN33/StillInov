de84a478955226ccdb320d7d8f66deaf69721d57
SDN33 <s.deinegri2@gmail.com>
2024-08-30 12:53:44 +0200

    stillinov

--- components/features.tsx
+++ components/features.tsx
@@ -16,11 +16,11 @@ export default function Features() {
 
             {/* 1st item */}
             <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
-              <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-                <rect className="fill-current text-purple-600" width="64" height="64" rx="32" />
-                <path className="stroke-current text-purple-100" d="M30 39.313l-4.18 2.197L27 34.628l-5-4.874 6.91-1.004L32 22.49l3.09 6.26L42 29.754l-3 2.924" strokeLinecap="square" strokeWidth="2" fill="none" fillRule="evenodd" />
-                <path className="stroke-current text-purple-300" d="M43 42h-9M43 37h-9" strokeLinecap="square" strokeWidth="2" />
-              </svg>
+            <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+              <rect className="fill-current text-blue-600" width="64" height="64" rx="32" />
+              <image href="/public/images/idvisuelle.svg" x="16" y="16" width="32" height="32" />
+            </svg>
+
               <h4 className="h4 mb-2">Identitée Visuelle</h4>
               <p className="text-lg text-gray-400 text-center"> Création d'une image de marque unique et cohérente pour renforcer votre reconnaissance et impact sur tous supports.</p>
             </div>
