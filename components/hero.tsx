b5484678a3f38335f13815281ebffd7d5be8a7d6
SDN33 <s.deinegri2@gmail.com>
2024-08-30 19:41:01 +0200

    stillinov

--- components/hero.tsx
+++ components/hero.tsx
@@ -18,12 +18,14 @@ export default function Hero() {
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
             <h1 className="h1 mb-4" data-aos="fade-up">
-              Agence Digitale et
+              Agence Digitale
               <span className="animated-text">
+                et
                 <span>Innovante 🚀</span>
                 <span>Ambicieuse 💪</span>
                 <span>Technologique 🧪</span>
-                <span>Fun</span>
+                <span>Fun 😎</span>
+                <span>Créative 🎨</span>
               </span>
             </h1>
             <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Donnez vie à vos ambitions digitales et transformez vos idées en réalité.</p>
