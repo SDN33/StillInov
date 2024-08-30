35b18d223e035ff2bf23e936190b90bf81378b90
SDN33 <s.deinegri2@gmail.com>
2024-08-30 20:00:30 +0200

    stillinov

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,9 +1,9 @@
 export const metadata = {
-  title: 'StillInov | Agence de Communication Digitale - Création, Design & Développement Web',
+  title: 'StillInov Agency | Agence de Communication Digitale - Création, Design & Développement Web',
   description: 'StillInov, votre partenaire en communication digitale, offre des services personnalisés de création, design, et développement web pour transformer vos idées en réalité, quel que soit votre budget.',
   keywords: 'communication digitale, création site web, design graphique, développement web, SEO, marketing digital, agence web, solutions personnalisées, agence communication digitale, audit marketing, référencement SEO, SEA, stratégies digitales,faire pub insta, faire pub facebook, faire pub google, faire audit',
   openGraph: {
-    title: 'StillInov | Agence de Communication Digitale',
+    title: 'StillInov Agency | Agence de Communication Digitale',
     description: 'Découvrez comment StillInov transforme vos idées en projets digitaux réussis grâce à des solutions personnalisées adaptées à vos besoins et à votre budget.',
     url: 'https://www.stillinov.com',
     type: 'website',
@@ -12,14 +12,14 @@ export const metadata = {
         url: 'https://www.stillinov.com/images/logo.png', // Assurez-vous d'avoir une image dédiée pour Open Graph
         width: 1200,
         height: 630,
-        alt: 'StillInov - Agence de Communication Digitale',
+        alt: 'StillInov Agency - Agence de Communication Digitale',
       },
     ],
   },
   twitter: {
     card: 'summary_large_image',
     site: '@StillInov',
-    title: 'StillInov | Agence de Communication Digitale - Création, Design & Développement Web',
+    title: 'StillInov Agency | Agence de Communication Digitale - Création, Design & Développement Web',
     description: 'StillInov, votre partenaire en communication digitale, offre des services personnalisés de création, design, et développement web pour transformer vos idées en réalité.',
   },
 };
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,30 @@
+export const metadata = {
+  title: 'StillInov Agency | Contactez-nous',
+  description: 'StillInov, votre partenaire en communication digitale, offre des services personnalisés de création, design, et développement web pour transformer vos idées en réalité, quel que soit votre budget.',
+  keywords: 'communication digitale, création site web, design graphique, développement web, SEO, marketing digital, agence web, solutions personnalisées, agence communication digitale, audit marketing, référencement SEO, SEA, stratégies digitales,faire pub insta, faire pub facebook, faire pub google, faire audit',
+  openGraph: {
+    title: 'StillInov Agency | Agence de Communication Digitale',
+    description: 'Découvrez comment StillInov transforme vos idées en projets digitaux réussis grâce à des solutions personnalisées adaptées à vos besoins et à votre budget.',
+    url: 'https://www.stillinov.com',
+    type: 'website',
+    images: [
+      {
+        url: 'https://www.stillinov.com/images/logo.png', // Assurez-vous d'avoir une image dédiée pour Open Graph
+        width: 1200,
+        height: 630,
+        alt: 'StillInov - Agence de Communication Digitale',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@StillInov',
+    title: 'StillInov Agency | Agence de Communication Digitale - Création, Design & Développement Web',
+    description: 'StillInov, votre partenaire en communication digitale, offre des services personnalisés de création, design, et développement web pour transformer vos idées en réalité.',
+  },
+};
+
+
 import React from 'react';
 
 // Define the ContactForm component
