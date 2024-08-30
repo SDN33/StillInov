db5fc2dd9bbd7c9bf332d0435ee654c115d7b725
SDN33 <s.deinegri2@gmail.com>
2024-08-30 15:19:28 +0200

    stillinov

--- components/ui/mobile-menu.tsx
+++ components/ui/mobile-menu.tsx
@@ -68,7 +68,6 @@ export default function MobileMenu() {
           <li>
             <Link
               href="/"
-              href="#services"
               className="flex font-medium w-full text-purple-600 hover:text-gray-200 py-2 justify-center" onClick={() => setMobileNavOpen(false)}>
               Services
             </Link>
