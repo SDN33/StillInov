234cbd7bcd3f7a546496fcaa7792d61166f0e9f5
SDN33 <s.deinegri2@gmail.com>
2024-08-31 20:35:57 +0200

    stillinov

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -73,6 +73,7 @@ export default function Testimonials() {
               <div className="border-t border-gray-800 mb-16"></div>
 
               <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+                <h2 className="h2 mb-4">Chez Still-Inov</h2>
                 <h2 className="h2 mb-4">Nos valeurs, gages de qualité</h2>
               </div>
 
