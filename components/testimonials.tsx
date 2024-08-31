9b6df7e39d1c70474a11d3c54ec23a77b4c1a3bf
SDN33 <s.deinegri2@gmail.com>
2024-08-31 20:23:07 +0200

    stillinov

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -87,8 +87,9 @@ export default function Testimonials() {
               <br />
 
               <div className="flex flex-wrap justify-center gap-12">
-                <div className="flex flex-col items-center max-w-sm hover:scale-110 transition transform duration-200">
-                  <div className="icon-wrapper p-4 rounded-full mb-4 bg-[#485b51] hover:bg-[#f15e4b] hover:scale-110 transition transform duration-200" style={{ fill: "#485b51" }}>
+                {/* Confiance */}
+                <div className="flex flex-col items-center max-w-sm hover:scale-110 transition-transform duration-200">
+                  <div className="icon-wrapper p-4 rounded-full mb-4 bg-[#485b51] hover:bg-[#f15e4b] transition-transform duration-200" style={{ fill: "#485b51" }}>
                     <svg className="w-16 h-16 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
                     </svg>
@@ -97,8 +98,9 @@ export default function Testimonials() {
                   <p className="text-center text-gray-400">Nous bâtissons des relations durables avec nos clients, basées sur la transparence, l'intégrité et des résultats concrets.</p>
                 </div>
 
-                <div className="flex flex-col items-center max-w-sm hover:scale-110 transition transform duration-200">
-                  <div className="icon-wrapper p-4 rounded-full mb-4 bg-[#485b51] hover:bg-[#f15e4b] hover:scale-110 transition transform duration-200" style={{ fill: "#485b51" }}>
+                {/* Qualité */}
+                <div className="flex flex-col items-center max-w-sm hover:scale-110 transition-transform duration-200">
+                  <div className="icon-wrapper p-4 rounded-full mb-4 bg-[#485b51] hover:bg-[#f15e4b] transition-transform duration-200" style={{ fill: "#485b51" }}>
                     <svg className="w-16 h-16 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
                     </svg>
@@ -107,8 +109,9 @@ export default function Testimonials() {
                   <p className="text-center text-gray-400">Notre expertise et notre attention aux détails garantissent des solutions de haute qualité qui dépassent vos attentes.</p>
                 </div>
 
-                <div className="flex flex-col items-center max-w-sm hover:scale-110 transition transform duration-200">
-                  <div className="icon-wrapper p-4 rounded-full mb-4  bg-[#485b51] hover:bg-[#f15e4b] hover:scale-110 transition transform duration-200" style={{ fill: "#485b51" }}>
+                {/* Adaptation */}
+                <div className="flex flex-col items-center max-w-sm hover:scale-110 transition-transform duration-200">
+                  <div className="icon-wrapper p-4 rounded-full mb-4 bg-[#485b51] hover:bg-[#f15e4b] transition-transform duration-200" style={{ fill: "#485b51" }}>
                     <svg className="w-16 h-16 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
                     </svg>
@@ -116,13 +119,6 @@ export default function Testimonials() {
                   <h4 className="text-xl font-semibold mb-3 text-white">Adaptation</h4>
                   <p className="text-center text-gray-400">Notre approche flexible nous permet de nous adapter à tout type de marché et de répondre efficacement à vos besoins spécifiques.</p>
                 </div>
-
-                <h3 className='mt-4 mb-12 text-xl text-gray-300 text-center max-w-3xl mx-auto'>
-                  Nous croyons que chaque projet est unique, c'est pourquoi nous offrons des solutions personnalisées adaptées à vos besoins spécifiques.
-                  <br /><br />
-                  Que vous ayez un petit budget ou de grandes ambitions, nous nous engageons à optimiser chaque aspect de votre projet pour vous offrir le meilleur résultat possible.
-                  <br /><br />L'équipe Still-Inov 🤝
-                </h3>
               </div>
             </div>
           </div>
