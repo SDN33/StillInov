2847fd506d5f6c83ee03821c72e1ab61a965ef05
SDN33 <s.deinegri2@gmail.com>
2024-08-31 11:26:07 +0200

    stillinov

--- components/features.tsx
+++ components/features.tsx
@@ -6,16 +6,22 @@ export default function Features() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">Atteignez des objectifs qui comptent</div>
-            <h2 className="h2 mb-4">Une solution complète pour renforcer votre empreinte numérique.</h2>
-            <p className="text-xl text-gray-400">De la création de sites web optimisés et attractifs à la gestion des réseaux sociaux, en passant par des stratégies de marketing digital personnalisées, nous donnons vie à vos ambitions digitales.</p>
+            <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">
+              Atteignez des objectifs qui comptent
+            </div>
+            <h2 className="h2 mb-4">
+              Une solution complète pour renforcer votre empreinte numérique.
+            </h2>
+            <p className="text-xl text-gray-400">
+              De la création de sites web optimisés et attractifs à la gestion des réseaux sociaux, en passant par des stratégies de marketing digital personnalisées, nous donnons vie à vos ambitions digitales.
+            </p>
           </div>
 
           {/* Items */}
           <div className="max-w-sm mx-auto grid gap-8 md:grid-cols-2 lg:grid-cols-3 lg:gap-16 items-start md:max-w-2xl lg:max-w-none" data-aos-id-blocks>
 
             {/* 1st item */}
-            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
+            <div className="relative flex flex-col items-center transform transition-transform duration-300 hover:scale-105" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
               <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
                 <rect className="fill-current text-purple-600" width="64" height="64" rx="32" />
                 <ellipse cx="32" cy="32" rx="20" ry="12" fill="#C4B5FD" className="stroke-current text-purple-300" strokeWidth="2" />
@@ -23,22 +29,26 @@ export default function Features() {
                 <circle cx="35" cy="29" r="3" fill="#EDE9FE" />
               </svg>
               <h4 className="h4 mb-2">Identité Visuelle</h4>
-              <p className="text-lg text-gray-400 text-center">Création d'une image de marque unique et cohérente pour renforcer votre reconnaissance et impact sur tous supports.</p>
+              <p className="text-lg text-gray-400 text-center">
+                Création d'une image de marque unique et cohérente pour renforcer votre reconnaissance et impact sur tous supports.
+              </p>
             </div>
 
             {/* 2nd item */}
-            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="100" data-aos-anchor="[data-aos-id-blocks]">
+            <div className="relative flex flex-col items-center transform transition-transform duration-300 hover:scale-105" data-aos="fade-up" data-aos-delay="100" data-aos-anchor="[data-aos-id-blocks]">
               <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
                 <rect className="fill-current text-purple-600" width="64" height="64" rx="32" />
                 <path className="stroke-current text-purple-100" strokeWidth="2" strokeLinecap="square" d="M21 23h22v18H21z" fill="none" fillRule="evenodd" />
                 <path className="stroke-current text-purple-300" d="M26 28h12M26 32h12M26 36h5" strokeWidth="2" strokeLinecap="square" />
               </svg>
               <h4 className="h4 mb-2">Site web et Application</h4>
-              <p className="text-lg text-gray-400 text-center">Concevoir des plateformes digitales sur mesure, alliant design moderne et performance, pour offrir une expérience utilisateur optimale.</p>
+              <p className="text-lg text-gray-400 text-center">
+                Concevoir des plateformes digitales sur mesure, alliant design moderne et performance, pour offrir une expérience utilisateur optimale.
+              </p>
             </div>
 
             {/* 3rd item */}
-            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="200" data-aos-anchor="[data-aos-id-blocks]">
+            <div className="relative flex flex-col items-center transform transition-transform duration-300 hover:scale-105" data-aos="fade-up" data-aos-delay="200" data-aos-anchor="[data-aos-id-blocks]">
               <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
                 <rect className="fill-current text-purple-600" width="64" height="64" rx="32" />
                 <g transform="translate(21 21)" strokeLinecap="square" strokeWidth="2" fill="none" fillRule="evenodd">
@@ -48,11 +58,13 @@ export default function Features() {
                 </g>
               </svg>
               <h4 className="h4 mb-2">Référencement SEO</h4>
-              <p className="text-lg text-gray-400 text-center">Optimiser votre visibilité en ligne pour vous positionner en tête des résultats de recherche et attirer plus de trafic qualifié.</p>
+              <p className="text-lg text-gray-400 text-center">
+                Optimiser votre visibilité en ligne pour vous positionner en tête des résultats de recherche et attirer plus de trafic qualifié.
+              </p>
             </div>
 
             {/* 4th item */}
-            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="300" data-aos-anchor="[data-aos-id-blocks]">
+            <div className="relative flex flex-col items-center transform transition-transform duration-300 hover:scale-105" data-aos="fade-up" data-aos-delay="300" data-aos-anchor="[data-aos-id-blocks]">
               <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
                 <rect className="fill-current text-purple-600" width="64" height="64" rx="32" />
                 <g transform="translate(22 21)" strokeLinecap="square" strokeWidth="2" fill="none" fillRule="evenodd">
@@ -61,11 +73,13 @@ export default function Features() {
                 </g>
               </svg>
               <h4 className="h4 mb-2">Stratégies Marketing</h4>
-              <p className="text-lg text-gray-400 text-center">Développer des plans sur mesure pour maximiser votre visibilité, attirer votre cible et stimuler votre croissance.</p>
+              <p className="text-lg text-gray-400 text-center">
+                Développer des plans sur mesure pour maximiser votre visibilité, attirer votre cible et stimuler votre croissance.
+              </p>
             </div>
 
             {/* 5th item */}
-            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="400" data-aos-anchor="[data-aos-id-blocks]">
+            <div className="relative flex flex-col items-center transform transition-transform duration-300 hover:scale-105" data-aos="fade-up" data-aos-delay="400" data-aos-anchor="[data-aos-id-blocks]">
               <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
                 <rect className="fill-current text-purple-600" width="64" height="64" rx="32" />
                 <g strokeLinecap="square" strokeWidth="2" fill="none" fillRule="evenodd">
@@ -74,11 +88,13 @@ export default function Features() {
                 </g>
               </svg>
               <h4 className="h4 mb-2">Campagnes Pub</h4>
-              <p className="text-lg text-gray-400 text-center">Créer des campagnes publicitaires efficaces pour accroître votre visibilité et générer des conversions.</p>
+              <p className="text-lg text-gray-400 text-center">
+                Créer des campagnes publicitaires efficaces pour accroître votre visibilité et générer des conversions.
+              </p>
             </div>
 
             {/* 6th item */}
-            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="500" data-aos-anchor="[data-aos-id-blocks]">
+            <div className="relative flex flex-col items-center transform transition-transform duration-300 hover:scale-105" data-aos="fade-up" data-aos-delay="500" data-aos-anchor="[data-aos-id-blocks]">
               <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
                 <rect className="fill-current text-purple-600" width="64" height="64" rx="32" />
                 <g transform="translate(21 22)" strokeLinecap="square" strokeWidth="2" fill="none" fillRule="evenodd">
@@ -88,7 +104,9 @@ export default function Features() {
                 </g>
               </svg>
               <h4 className="h4 mb-2">Audit</h4>
-              <p className="text-lg text-gray-400 text-center">Évaluer et optimiser vos performances digitales pour identifier les opportunités d'amélioration.</p>
+              <p className="text-lg text-gray-400 text-center">
+                Évaluer et optimiser vos performances digitales pour identifier les opportunités d'amélioration.
+              </p>
             </div>
 
           </div>
