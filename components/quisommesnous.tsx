abe30c2f4193c045cc66eceb27166877d0d1112b
SDN33 <s.deinegri2@gmail.com>
2024-08-31 20:18:29 +0200

    stillinov

--- components/quisommesnous.tsx
+++ components/quisommesnous.tsx
@@ -14,7 +14,7 @@ const TeamMember = ({ image, name, role, description, linkedinUrl }: { image: st
     </div>
     <p className="text-lg text-gray-400">{description}</p>
     <div className="text-gray-200 font-medium mt-4">
-      {name} - <span className="text-[#485b51] hover:text-gray-200 transition duration-150 ease-in-out">{role}</span>
+      {name} - <span className={name === "Stéphane Dei-negri" ? "text-[#f15e4b]" : "text-[#485b51]"}>{role}</span>
     </div>
   </div>
 );
