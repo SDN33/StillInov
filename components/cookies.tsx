472e1780fcf186d8fc71fb26f92f85a6f1b0b2d5
SDN33 <s.deinegri2@gmail.com>
2024-09-01 12:51:57 +0200

    stillinov

--- components/cookies.tsx
+++ components/cookies.tsx
@@ -39,7 +39,7 @@ const CookieBanner = () => {
   if (!showBanner) return null;
 
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-[#485b51e6] text-white p-4 shadow-lg z-50">
+    <div className="fixed bottom-0 left-0 right-0 bg-gradient-to-b from-[#f37335] via-[#f15e4b] to-[#90e7d3] text-white p-4 shadow-lg z-50">
       <div className="container mx-auto flex flex-col sm:flex-row justify-between items-center">
         <p className="text-sm mb-4 sm:mb-0 sm:mr-4 text-center sm:text-left">
           Nous utilisons des cookies pour améliorer votre expérience sur notre site.
