a5f279ca3a88b693de9c95b084e2197a258e9ab1
SDN33 <s.deinegri2@gmail.com>
2024-09-01 17:24:38 +0200

    stillinov

--- app/contact/contact-form.tsx
+++ app/contact/contact-form.tsx
@@ -10,7 +10,7 @@ const ContactForm: React.FC = () => {
             <h3 className="text-2xl font-semibold text-[#f15e4b] mb-2">Contactez-nous</h3>
             <p className="text-gray-700">
               Envoyez-nous un message, nous serons ravis de vous répondre dans les plus brefs délais. 🚀
-              <br /> Vous pouvez également nous contacter par email à <a className="hover:text-[#485b51]" href="mailto:stillinovagency@gmail.com " className="text-[#f15e4b]"> Stillinovagency@gmail.com</a>.
+              <br /> Vous pouvez également nous contacter par email à <a href="mailto:stillinovagency@gmail.com " className="text-[#f15e4b] hover:text-[#485b51]"> Stillinovagency@gmail.com</a>.
 
             </p>
           </div>
