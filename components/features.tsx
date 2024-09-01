7f110e143f7575b1412bda6db3e2f69eab2163ca
SDN33 <s.deinegri2@gmail.com>
2024-09-02 00:41:02 +0200

    stillinov

--- components/features.tsx
+++ components/features.tsx
@@ -184,7 +184,7 @@ export default function Features() {
             </div>
 
           </div>
-          <br />
+          <br /><br />
           <a
             className="btn text-center text-white w-3 mt-6 mb-4 sm:w-auto sm:mb-0 bg-[#485b51] hover:bg-gray-800 shadow mx-auto flex justify-center"
             href="/contact"
