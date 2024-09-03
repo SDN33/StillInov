b380abd89ab65341cf1d1a989b9e8f609b187eda
SDN33 <s.deinegri2@gmail.com>
2024-09-03 09:18:56 +0200

    stillinov

--- components/features.tsx
+++ components/features.tsx
@@ -186,7 +186,7 @@ export default function Features() {
           </div>
           <br /><br />
           <a
-            className="btn text-center text-white w-full mt-8 mb-4 sm:w-auto sm:mb-0 bg-[#485b51] hover:bg-gray-800 shadow mx-auto flex justify-center"
+            className="btn text-center text-white w-full mt-8 mb-4 sm:w-auto sm:mb-0 bg-[#f15e4b] hover:bg-gray-800 shadow mx-auto flex justify-center"
             href="/contact"
           >
             Planifier un appel téléphonique 📞
