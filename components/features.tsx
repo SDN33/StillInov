20fe122c7687a21c17195e5ffe2b7ffb62247a9a
SDN33 <s.deinegri2@gmail.com>
2024-09-03 10:05:53 +0200

    stillinov

--- components/features.tsx
+++ components/features.tsx
@@ -1,4 +1,10 @@
+import React from 'react';
+
 export default function Features() {
+  const customButtonStyle = {
+    background: 'rgb(243,115,53)',
+    backgroundImage: 'radial-gradient(circle, rgba(243,115,53,1) 0%, rgba(241,94,75,1) 35%, rgba(144,231,211,1) 100%)',
+  };
   return (
     <section>
       <div className="max-w-6xl mx-auto px-4 sm:px-6" id="features">
@@ -186,8 +192,9 @@ export default function Features() {
           </div>
           <br /><br />
           <a
-            className="btn text-center text-white w-full mt-8 mb-4 sm:w-auto sm:mb-0 bg-[#f15e4b] hover:bg-gray-800 shadow mx-auto flex justify-center "
+            className="btn text-center text-white w-full mt-8 mb-4 sm:w-auto sm:mb-0 hover:bg-gray-800 shadow mx-auto flex justify-center"
             href="https://calendly.com/stillinovagency/30min"
+            style={customButtonStyle}
           >
             Planifier un appel téléphonique 📞
           </a>
