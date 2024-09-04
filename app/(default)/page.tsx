50135ea6428463479875138a6a1f8bb87679fcd9
SDN33 <s.deinegri2@gmail.com>
2024-09-04 21:10:21 +0200

    stillinov

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,4 +1,31 @@
-import Head from 'next/head'
+
+export const metadata = {
+  title: 'Still-Inov Agency | Agence de Communication & Marketing Digitale - Développement Web, SEO & Réseaux Sociaux',
+  description: 'Still-Inov, votre partenaire en communication digitale, offre des services personnalisés de création, design, et développement web pour transformer vos idées en réalité, quel que soit votre budget.',
+  keywords: 'communication digitale, création site web, design graphique, développement web, SEO, marketing digital, agence web, agence marketing bordeaux, agence communication bordeaux , agence communication digitale, audit marketing, référencement SEO, SEA, stratégies digitales,faire pub insta, faire pub facebook, faire pub google, faire audit',
+  openGraph: {
+    title: 'Still-Inov Agency | Agence de Communication & Marketing Digitale',
+    description: 'Découvrez comment Still-Inov transforme vos idées en projets digitaux réussis grâce à des solutions personnalisées adaptées à vos besoins et à votre budget.',
+    url: 'https://www.stillinov.com',
+    type: 'website',
+    images: [
+      {
+        url: 'https://www.stillinov.com/images/banner.png', // Assurez-vous d'avoir une image dédiée pour Open Graph
+        width: 1200,
+        height: 630,
+        alt: 'Still-Inov Agency - Agence de Communication & Marketing Digitale',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@StillInov',
+    title: 'Still-Inov Agency | Agence de Communication & Marketing Digitale - Développement Web, SEO & Réseaux Sociaux',
+    description: 'Still-Inov, votre partenaire en communication digitale, offre des services personnalisés de création, design, et développement web pour transformer vos idées en réalité.',
+  },
+
+};
+
 import Hero from '@/components/hero'
 import Features from '@/components/features'
 import Newsletter from '@/components/newsletter'
@@ -8,48 +35,19 @@ import Cookies from '@/components/cookies'
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
+
+
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Still-Inov Agency | Agence de Communication & Marketing Digitale - Développement Web, SEO & Réseaux Sociaux</title>
-        <meta name="description" content="Still-Inov, votre partenaire en communication digitale, offre des services personnalisés de création, design, et développement web pour transformer vos idées en réalité, quel que soit votre budget." />
-        <meta name="keywords" content="communication digitale, création site web, design graphique, développement web, SEO, marketing digital, agence web, agence marketing bordeaux, agence communication bordeaux , agence communication digitale, audit marketing, référencement SEO, SEA, stratégies digitales,faire pub insta, faire pub facebook, faire pub google, faire audit" />
-
-        <meta property="og:title" content="Still-Inov Agency | Agence de Communication & Marketing Digitale" />
-        <meta property="og:description" content="Découvrez comment Still-Inov transforme vos idées en projets digitaux réussis grâce à des solutions personnalisées adaptées à vos besoins et à votre budget." />
-        <meta property="og:url" content="https://www.stillinov.com" />
-        <meta property="og:image" content="https://www.stillinov.com/images/banner.png" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta property="og:type" content="website" />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@StillInov" />
-        <meta name="twitter:title" content="Still-Inov Agency | Agence de Communication & Marketing Digitale - Développement Web, SEO & Réseaux Sociaux" />
-        <meta name="twitter:description" content="Still-Inov, votre partenaire en communication digitale, offre des services personnalisés de création, design, et développement web pour transformer vos idées en réalité." />
-        <meta name="twitter:image" content="https://www.stillinov.com/images/banner.png" />
-
-        <script type="application/ld+json">
-        {`
-          {
-            "@type": "Organization",
-            "name": "Still-Inov Agency",
-            "url": "https://www.stillinov.com",
-            "logo": "https://www.stillinov.com/images/logo.png"
-          }
-        `}
-        </script>
-      </Head>
-
       <Hero />
       <Features />
       <AboutUs />
       <Testimonials />
       <Newsletter />
       <Cookies />
-      <Analytics />
+      <Analytics  />
       <SpeedInsights />
-    </>
-  )
+
+    </>)
 }
