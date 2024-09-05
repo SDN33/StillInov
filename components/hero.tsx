5c7dbc471052935e69cb61ab01906469064f4cb6
SDN33 <s.deinegri2@gmail.com>
2024-09-05 21:13:41 +0200

    stillinov

--- components/hero.tsx
+++ components/hero.tsx
@@ -35,7 +35,7 @@ export default function Hero() {
                   className="btn text-white w-full mb-4 sm:w-auto sm:mb-0"
                   href="/contact"
                   style={{ backgroundColor: '#f15e4b' }}
-                  onMouseEnter={e => e.currentTarget.style.backgroundColor = '#2d3748'}
+                  onMouseEnter={e => e.currentTarget.style.backgroundColor = '#1a202c'}
                   onMouseLeave={e => e.currentTarget.style.backgroundColor = '#f15e4b'}
                 >
                   Contactez-nous
