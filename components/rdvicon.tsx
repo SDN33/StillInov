3650cd0eb336ea5c3fa66f55c2e055ac642d15f2
SDN33 <s.deinegri2@gmail.com>
2024-09-06 14:21:08 +0200

    stillinov

--- components/rdvicon.tsx
+++ components/rdvicon.tsx
@@ -48,7 +48,7 @@ const RdvIcon: React.FC = () => {
               >
                 &times;
               </button>
-              <h2 className="text-xl font-bold mb-4 text-center">Pourquoi plannifier un appel ?</h2>
+              <h2 className="text-xl font-bold mb-4">Pourquoi plannifier un appel ?</h2>
               <p className="mb-4 text-center">
                 Pour nous chaque client benificie d'une attention particulière et d'un suivi personnalisé.
                 Nous vous offrons un appel de présentation de 15 minutes gratuit pour discuter de vos besoins et vous expliquer comment nous pouvons vous aider à atteindre vos objectifs. 🚀
