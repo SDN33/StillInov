bde8dae509af8b3e7d84715f21be59c62d8d2043
SDN33 <s.deinegri2@gmail.com>
2024-09-06 13:55:10 +0200

    stillinov

--- components/rdvicon.tsx
+++ components/rdvicon.tsx
@@ -8,10 +8,10 @@ const RdvIcon: React.FC = () => {
   return (
     <div className="fixed bottom-4 right-4 md:bottom-6 md:right-6">
       <div className="relative">
-        <button 
+        <button
           onMouseEnter={() => setIsHovered(true)}
           onMouseLeave={() => setIsHovered(false)}
-          className="bg-green-500 p-3 rounded-full shadow-lg text-white hover:bg-green-600 transition-colors"
+          className="p-3 rounded-full shadow-lg text-white bg-gradient-to-br from-orange-400 via-red-500 to-teal-400"
           aria-label="Contactez-nous"
         >
           <svg
@@ -28,7 +28,7 @@ const RdvIcon: React.FC = () => {
           </svg>
         </button>
         {isHovered && (
-          <div className="absolute bottom-14 right-1/2 transform translate-x-1/2 bg-gray-800 text-white text-xs rounded py-1 px-2 shadow-lg whitespace-nowrap">
+          <div className="absolute bottom-14 right-1/2 transform translate-x-1/2 bg-gray-800 text-white text-xs rounded py-1 px-2 shadow-lg whitespace-nowrap hidden md:block">
             Bénéficiez d'un appel de présentation de 15 min gratuit
           </div>
         )}
