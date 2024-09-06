7207931e050426412540d75601f5b48195ae59b9
SDN33 <s.deinegri2@gmail.com>
2024-09-06 16:55:31 +0200

    stillinov

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -79,11 +79,31 @@ export default function Testimonials() {
               </div>
               <blockquote className="text-lg text-gray-400 grow">— J'ai fait appel à l'agence pour concevoir l'intégralité de mon identité visuelle, du logo aux flyers. Aujourd'hui, après plus de cinq ans d'existence, mon bar est devenu une adresse incontournable à Bègles. Je recommande vivement leurs services !</blockquote>
               <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
-                <cite className="text-gray-200 not-italic">Emmanuel D.</cite> - <a className="text-[#f15e4b] hover:text-gray-200 transition duration-150 ease-in-out" href="https://www.lescordeliers.com">Les Cordeliers</a>
+                <cite className="text-gray-200 not-italic">Cédric Césarini</cite> - <a className="text-[#f15e4b] hover:text-gray-200 transition duration-150 ease-in-out" href="https://www.facebook.com/bewinebegles">Be Wine Bègles</a>
               </div>
             </div>
           </div>
         </div>
+
+        {/* Google Reviews */}
+        {reviews.map((review, index) => (
+            <div key={index} className="group flex flex-col h-full p-6 bg-gray-800 transition-transform transform duration-200 hover:scale-105">
+              <div>
+                <div className="relative inline-flex flex-col mb-4">
+                  <Image className="rounded-full" src={review.profile_photo_url} width={48} height={48} alt={review.author_name} />
+                  <svg className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current" viewBox="0 0 24 20" xmlns="http://www.w3.org/2000/svg" style={{ fill: "#f15e4b" }}>
+                    <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
+                  </svg>
+                </div>
+              </div>
+              <blockquote className="text-lg text-gray-400 grow">— {review.text}</blockquote>
+              <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
+                <cite className="text-gray-200 not-italic">{review.author_name}</cite> - {review.relative_time_description}
+              </div>
+            </div>
+          ))}
+        </div>
+
 
         {/* Additional Information */}
         <div className="bg-gray-900 py-16">
