1a04a85dd474c09efe8e049483b28284cf4a08d7
SDN33 <s.deinegri2@gmail.com>
2024-09-06 12:11:17 +0200

    stillinov

--- components/features.tsx
+++ components/features.tsx
@@ -217,7 +217,7 @@ const Features: React.FC = () => {
             Planifiez un appel téléphonique gratuit 📞
           </a>
           <a
-            className="text-lg text-gray-400 text-center"
+            className="text-lg text-gray-400 text-center  mx-auto"
             href="https://calendly.com"
           >
              <em>powered by Calendly</em>
