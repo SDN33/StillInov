e02f75116a411eaafa7d8e790e2bd524c7896222
SDN33 <s.deinegri2@gmail.com>
2024-09-06 17:22:28 +0200

    stillinov

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -1,38 +1,9 @@
-"use client";
-
-import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import TestimonialImage01 from '@/public/images/testimonial-01.png';
 import TestimonialImage02 from '@/public/images/testimonial-02.jpg';
 import TestimonialImage03 from '@/public/images/testimonial-03.jpg';
 
-// Définir le type pour les avis Google
-interface Review {
-  profile_photo_url: string;
-  author_name: string;
-  text: string;
-  relative_time_description: string;
-}
-
-// Suite des sections des témoignages
-
 export default function Testimonials() {
-  const [reviews, setReviews] = useState<Review[]>([]);
-
-  useEffect(() => {
-    async function fetchReviews() {
-      try {
-        const response = await fetch('/api/googleReviews');
-        const data = await response.json();
-        setReviews(data);
-      } catch (error) {
-        console.error('Error fetching Google reviews:', error);
-      }
-    }
-
-    fetchReviews();
-  }, []);
-
   return (
     <section>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
@@ -40,11 +11,12 @@ export default function Testimonials() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h2 className="h2 mb-4">Parce qu'un avis<br /> vaut mieux qu'un<br /> long discours !</h2>
+            <h2 className="h2 mb-4">Parce qu'un avis<br /> vaut mieux qu'un<br />long discours !</h2>
           </div>
 
           {/* Testimonials */}
           <div className="max-w-sm mx-auto grid gap-8 lg:grid-cols-3 lg:gap-6 items-start lg:max-w-none">
+
             {/* 1st testimonial */}
             <div className="group flex flex-col h-full p-6 bg-gray-800 transition-transform transform duration-200 hover:scale-105" data-aos="fade-up">
               <div>
@@ -89,39 +61,64 @@ export default function Testimonials() {
               </div>
               <blockquote className="text-lg text-gray-400 grow">— J'ai fait appel à l'agence pour concevoir l'intégralité de mon identité visuelle, du logo aux flyers. Aujourd'hui, après plus de cinq ans d'existence, mon bar est devenu une adresse incontournable à Bègles. Je recommande vivement leurs services !</blockquote>
               <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
-                <cite className="text-gray-200 not-italic">Cédric Césarini</cite> - <a className="text-[#f15e4b] hover:text-gray-200 transition duration-150 ease-in-out" href="#0">L'Apollo</a>
+                <cite className="text-gray-200 not-italic">Cédric Césarini</cite> - <a className="text-[#f15e4b] hover:text-gray-200 transition duration-150 ease-in-out" href="https://www.facebook.com/bewinebegles">Be Wine Bègles</a>
               </div>
             </div>
           </div>
+          <br />
+          <br />
+          {/* Additional Information */}
+          <div className="bg-gray-900 py-16">
+            <div className="container mx-auto px-4">
+              <div className="border-t border-gray-800 mb-16"></div>
 
-          {/* Google Reviews Section */}
-          <div className="mt-12 md:mt-20">
-            <h3 className="text-2xl font-semibold text-gray-100 mb-6">Avis Google</h3>
-            <div className="space-y-8">
-              {reviews.length > 0 ? (
-                reviews.map((review, index) => (
-                  <div key={index} className="p-6 bg-gray-800 border border-gray-700 rounded-lg">
-                    <div className="flex items-center mb-4">
-                      <Image className="rounded-full" src={review.profile_image_url} width={48} height={48} alt={`Review from ${review.author_name}`} />
-                      <div className="ml-4">
-                        <h4 className="text-lg font-medium text-gray-200">{review.author_name}</h4>
-                        <div className="flex items-center space-x-1">
-                          <svg className="w-4 h-4 text-yellow-400" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M12 2C6.48 2 2 6.48 2 12c0 2.62 1.1 5.17 2.89 7.06L12 22l7.11-2.94C20.9 17.17 22 14.62 22 12c0-5.52-4.48-10-10-10zm0 16.28c-.58 0-1.14-.2-1.57-.56l-5.13-3.55c-.55-.39-.82-1.08-.59-1.73.2-.62.75-1.07 1.39-1.12.1-.01.2-.01.31-.01.33 0 .66.1.94.28l2.78 1.93V8.9c0-.55.45-1 1-1s1 .45 1 1v4.53l2.78-1.93c.29-.19.61-.28.94-.28.11 0 .21 0 .31.01.64.05 1.2.5 1.39 1.12.23.65-.04 1.34-.59 1.73l-5.13 3.55c-.43.36-.99.56-1.57.56z" />
-                          </svg>
-                          {/* Add more stars if needed */}
-                        </div>
-                      </div>
-                    </div>
-                    <p className="text-gray-400">{review.text}</p>
+                        <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+                <h2 className="h2 mb-4">Chez Still-Inov Agency</h2>
+                <p className="text-lg text-gray-400">
+                  Nous croyons que chaque projet est unique, c'est pourquoi nous offrons des solutions personnalisées adaptées à vos besoins spécifiques.
+                </p>
+              </div>
+
+                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12">
+                {/* Bloc Confiance */}
+                <div className="group flex flex-col items-center max-w-sm transition-transform transform duration-200 hover:scale-110">
+                  <div className="icon-wrapper p-4 rounded-full mb-4 bg-[#485b51] group-hover:bg-[#f15e4b] transition-colors duration-200">
+                    <svg className="w-16 h-16 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
+                    </svg>
                   </div>
-                ))
-              ) : (
-                <p className="text-gray-400">Aucun avis Google disponible pour le moment.</p>
-              )}
+                  <h4 className="text-xl font-semibold mb-3 text-white group-hover:text-[#f15e4b] transition-colors duration-200">Confiance</h4>
+                  <p className="text-center text-gray-400">Nous bâtissons des relations durables avec nos clients, basées sur la transparence, l'intégrité et des résultats concrets.</p>
+                </div>
+
+                          {/* Bloc Qualité */}
+                <div className="group flex flex-col items-center max-w-sm transition-transform transform duration-200 hover:scale-110">
+                  <div className="icon-wrapper p-4 rounded-full mb-4 bg-[#485b51] group-hover:bg-[#f15e4b] transition-colors duration-200">
+                    <svg className="w-16 h-16 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
+                    </svg>
+                  </div>
+                  <h4 className="text-xl font-semibold mb-3 text-white group-hover:text-[#f15e4b] transition-colors duration-200">Qualité</h4>
+                  <p className="text-center text-gray-400">Notre expertise et notre attention aux détails garantissent des solutions de haute qualité qui dépassent vos attentes.</p>
+                </div>
+
+                          {/* Bloc Adaptation */}
+                <div className="group flex flex-col items-center max-w-sm transition-transform transform duration-200 hover:scale-110">
+                  <div className="icon-wrapper p-4 rounded-full mb-4 bg-[#485b51] group-hover:bg-[#f15e4b] transition-colors duration-200">
+                    <svg className="w-16 h-16 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
+                    </svg>
+                  </div>
+                  <h4 className="text-xl font-semibold mb-3 text-white group-hover:text-[#f15e4b] transition-colors duration-200">Adaptation</h4>
+                  <p className="text-center text-gray-400">Notre approche flexible nous permet de nous adapter à tout type de marché et de répondre efficacement à vos besoins spécifiques.</p>
+                </div>
+              </div>
+              <br /><br />
+              <p className="text-lg text-white-400 text-center">
+                L'équipe Still-Inov Agency 🤝
+              </p>
             </div>
           </div>
-
         </div>
       </div>
     </section>
