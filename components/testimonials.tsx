395e4f1663f53748d0dbb62b03058a0e45bf76a0
SDN33 <s.deinegri2@gmail.com>
2024-09-06 17:00:02 +0200

    stillinov

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -86,7 +86,8 @@ export default function Testimonials() {
         </div>
 
         {/* Google Reviews */}
-        {reviews.map((review, index) => (
+        <div className="max-w-sm mx-auto grid gap-8 lg:grid-cols-3 lg:gap-6 items-start lg:max-w-none">
+          {reviews.map((review, index) => (
             <div key={index} className="group flex flex-col h-full p-6 bg-gray-800 transition-transform transform duration-200 hover:scale-105">
               <div>
                 <div className="relative inline-flex flex-col mb-4">
@@ -103,7 +104,6 @@ export default function Testimonials() {
             </div>
           ))}
         </div>
-
 
         {/* Additional Information */}
         <div className="bg-gray-900 py-16">
