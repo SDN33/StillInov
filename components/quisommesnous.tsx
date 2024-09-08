665b8da6f79e3df2668a25afa5c21a57aa362136
SDN33 <s.deinegri2@gmail.com>
2024-09-08 20:11:38 +0200

    stillinov

--- components/quisommesnous.tsx
+++ components/quisommesnous.tsx
@@ -54,7 +54,7 @@ export default function AboutUs() {
           </div>
         </div>
       </div>
-      <a className="flex justify-center items-center text-white bg-[#0A66C2] hover:bg-[#004182] rounded-full w-48 h-10 font-semibold mx-auto align-items-center mt-[-3rem] sm:mt-[-1rem] " href="https://www.linkedin.com/comm/mynetwork/discovery-see-all?usecase=PEOPLE_FOLLOWS&followMember=stephane-dn" target="_blank" rel="noopener noreferrer">
+      <a className="flex justify-center items-center text-white bg-[#0A66C2] hover:bg-[#004182] rounded-full w-48 h-10 font-semibold mx-auto align-items-center mt-[-3rem] sm:mt-[2rem] " href="https://www.linkedin.com/comm/mynetwork/discovery-see-all?usecase=PEOPLE_FOLLOWS&followMember=stephane-dn" target="_blank" rel="noopener noreferrer">
         <Image src={LinkedInLogo} width={20} height={20} alt="LinkedIn" className="mr-2" />
         Follow on LinkedIn
       </a>
