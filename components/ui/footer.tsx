4c3cdef3dc0e442ecc81213f9de54d53b1888e93
SDN33 <s.deinegri2@gmail.com>
2024-09-08 19:24:39 +0200

    stillinov

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -67,7 +67,7 @@ export default function Footer() {
             <Link href="https://www.linkedin.com/company/still-inov-agency" passHref>
               <a target="_blank" rel="noopener noreferrer">
                 <Image
-                  src="/images/linkedin.png"
+                  src="/images/llinkedin.webp"
                   alt="LinkedIn"
                   width={24}
                   height={24}
