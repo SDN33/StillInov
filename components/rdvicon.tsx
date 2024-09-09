cbbcbbf41cb41ac17224459b33feb26dc22c625f
SDN33 <s.deinegri2@gmail.com>
2024-09-09 16:22:29 +0200

    stillinov

--- components/rdvicon.tsx
+++ components/rdvicon.tsx
@@ -121,16 +121,16 @@ const RdvIcon: React.FC = () => {
               </button>
               <h2 className="text-xl font-bold mb-2 text-center">Bénéficiez d'un appel gratuit !</h2>
               <br />
-              <div className="flex flex-col items-center bg-white p-6 rounded-lg shadow-lg transform transition-transform duration-300 hover:scale-105">
               <div className="relative mb-4">
                   <a>
-                    <Image className="rounded-full" src={Support} width={96} height={96} alt={`Photo de SAV`} />
+                    <Image className="rounded-full" src={Support} width={150} height={150} alt={`Photo de SAV`} />
                   </a>
                   <svg className={`absolute top-0 right-0 -mr-3 w-6 h-5 fill-current ${"text-[#f15e4b]"}`} viewBox="0 0 24 20" xmlns="http://www.w3.org/2000/svg">
                     <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
                   </svg>
                 </div>
               </div>
+              <br />
               <p className="mb-2 text-center">
                 Pour nous chaque client bénéficie d'une attention particulière et d'un suivi personnalisé.
                 <br />
