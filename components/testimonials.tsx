b5f5ce7e981babbf65584fe5923f1c60c85c25c0
SDN33 <s.deinegri2@gmail.com>
2024-09-10 02:23:55 +0200

    stillinov

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -9,7 +9,7 @@ export default function Testimonials() {
 
   return (
     <section>
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 mt-[-2rem]  ">
         <div className="py-12 md:py-20 border-t border-gray-800">
 
           {/* Section header */}
