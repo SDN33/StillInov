5e88a179b88f0a35a4bae321547554f36f8d0256
SDN33 <s.deinegri2@gmail.com>
2024-09-16 20:25:20 +0200

    si

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,91 +1,126 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
-import Script from 'next/script';
-
-// Définition du type pour les métadonnées Open Graph
-interface OGMetadata {
+// Define your Metadata type (if it's not available in your setup):
+export interface Metadata {
   title: string;
   description: string;
-  image: string;
-  url: string;
-  type: string;
+  keywords: string;
+  authors: {
+    name: string;
+  };
+  robots: string;
+  canonical: string;
+  openGraph: {
+    title: string;
+    description: string;
+    url: string;
+    type: string;
+    images: {
+      url: string;
+      width: number;
+      height: number;
+      alt: string;
+    }[];
+    locale: string;
+    siteName: string;
+  };
+  twitter: {
+    card: string;
+    site: string;
+    title: string;
+    description: string;
+    images: string;
+  };
+  additionalMeta: Record<string, string>;
+  structuredData: Record<string, any>;
+  favicon?: string;  // Optional favicon property
 }
 
-// Déclaration des métadonnées
-export const metadata: Metadata & { og: OGMetadata; twitter: { card: string; title: string; description: string; image: string } } = {
-  title: "Canva Pro Gratuit | Rejoignez la Communauté de Canva en France",
-  description: "Obtenez Canva Pro gratuitement avec notre communauté exclusive. Profitez des avantages de Canva Pro sans frais et découvrez comment notre communauté d'utilisateurs en France facilite l'accès à Canva Pro.",
-  keywords: "Canva Pro gratuit, Canva gratuit, obtenir Canva Pro gratuitement, communauté Canva, avantages Canva Pro, offre Canva Pro, Canva Pro France, Canva Pro sans frais, Canva Pro gratuit en France, avoir canva gratuit.",
-  viewport: "width=device-width, initial-scale=1.0",
-  robots: "index, follow",
-  og: {
-    title: "Canva Pro Gratuit | Rejoignez la Communauté de Canva en France",
-    description: "Obtenez Canva Pro gratuitement avec notre communauté exclusive. Profitez des avantages de Canva Pro sans frais et découvrez comment notre communauté d'utilisateurs en France facilite l'accès à Canva Pro.",
-    image: "https://digiload.online/images/banner.png",
-    url: "https://digiload.online",
-    type: "website",
+// Export metadata as an object:
+export const metadata: Metadata = {
+  title: 'Still-inov Agency | Agence de Marketing & Communication Digitale à Bordeaux - Développement Web, SEO, Réseaux Sociaux',
+  description: 'Still-inov, votre expert en marketing et communication digitale à Bordeaux...',
+  keywords: 'communication digitale Bordeaux, création site web Bordeaux, etc.',
+  authors: {
+    name: 'Still-inov Agency',
+  },
+  robots: 'index, follow',
+  canonical: 'https://www.stillinov.com',
+  openGraph: {
+    title: 'Still-inov Agency | Agence de Marketing & Communication Digitale à Bordeaux',
+    description: 'Découvrez comment Still-inov transforme vos idées...',
+    url: 'https://www.stillinov.com',
+    type: 'website',
+    images: [
+      {
+        url: 'https://www.stillinov.com/images/banner.png',
+        width: 1200,
+        height: 630,
+        alt: 'Still-inov Agency - Agence de Marketing & Communication Digitale à Bordeaux',
+      },
+    ],
+    locale: 'fr_FR',
+    siteName: 'Still-inov Agency',
   },
   twitter: {
-    card: "summary_large_image",
-    title: "Canva Pro Gratuit | Rejoignez la Communauté de Canva en France",
-    description: "Obtenez Canva Pro gratuitement avec notre communauté exclusive. Profitez des avantages de Canva Pro sans frais et découvrez comment notre communauté d'utilisateurs en France facilite l'accès à Canva Pro.",
-    image: "https://digiload.online/images/banner.png",
+    card: 'summary_large_image',
+    site: '@StillInov',
+    title: 'Still-inov Agency | Agence de Marketing & Communication Digitale à Bordeaux',
+    description: 'Still-inov, votre partenaire à Bordeaux...',
+    images: 'https://www.stillinov.com/images/banner.png',
   },
+  additionalMeta: {
+    'twitter:image:alt': 'Still-inov Agency - Agence de Marketing & Communication Digitale à Bordeaux',
+    'twitter:creator': '@StillInov',
+  },
+  structuredData: {
+    '@context': 'https://schema.org',
+    '@type': 'Organization',
+    name: 'Still-inov Agency',
+    url: 'https://www.stillinov.com',
+    logo: 'https://www.stillinov.com/images/logo.png',
+    description: 'Agence de Marketing & Communication Digitale...',
+    sameAs: [
+      'https://www.linkedin.com/company/still-inov',
+      'https://twitter.com/StillInov',
+      'https://www.facebook.com/StillInov',
+    ],
+    contactPoint: {
+      '@type': 'ContactPoint',
+      telephone: '+33-6-09-95-24-09',
+      contactType: 'Customer Service',
+      areaServed: 'FR',
+      availableLanguage: ['French'],
+    },
+  },
+  favicon: '/public/favicon.ico',  // Added favicon
 };
 
-// Composant racine de la page
-export default function RootLayout() {
+
+
+import dynamic from 'next/dynamic';
+import Hero from '@/components/hero';
+import Features from '@/components/features';
+import Rdvicon from '@/components/rdvicon';
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
+// Lazy load non-critical components
+const AboutUs = dynamic(() => import('@/components/quisommesnous'));
+const Testimonials = dynamic(() => import('@/components/testimonials'));
+const Newsletter = dynamic(() => import('@/components/newsletter'));
+const Cookies = dynamic(() => import('@/components/cookies'));
+
+export default function Home() {
   return (
-    <html lang="fr">
-      <head>
-        {/* Balises Open Graph */}
-        <meta property="og:image" content={metadata.og.image} />
-        <meta property="og:title" content={metadata.og.title} />
-        <meta property="og:description" content={metadata.og.description} />
-        <meta property="og:url" content={metadata.og.url} />
-        <meta property="og:type" content={metadata.og.type} />
-
-        {/* Balises Twitter */}
-        <meta name="twitter:card" content={metadata.twitter.card} />
-        <meta name="twitter:title" content={metadata.twitter.title} />
-        <meta name="twitter:description" content={metadata.twitter.description} />
-        <meta name="twitter:image" content={metadata.twitter.image} />
-
-        {/* Favicon */}
-        <link rel="icon" href="https://digiload.online/app/favicon.ico" type="image/x-icon" />
-
-        {/* Script Google Ads */}
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5355085288090957" crossOrigin="anonymous"></script>
-
-        {/* Script pour Brevo Conversations */}
-        <Script
-          id="brevo-conversations"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(d, w, c) {
-                w.BrevoConversationsID = '66e74748bc1eee12ff09caba';
-                w[c] = w[c] || function() {
-                  (w[c].q = w[c].q || []).push(arguments);
-                };
-                var s = d.createElement('script');
-                s.async = true;
-                s.src = 'https://conversations-widget.brevo.com/brevo-conversations.js';
-                if (d.head) d.head.appendChild(s);
-              })(document, window, 'BrevoConversations');
-            `,
-          }}
-        />
-      </head>
-      <body>
-        {/* Ajout du DotLottie Player */}
-        <Script
-          src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs"
-          type="module"
-          crossOrigin="anonymous"
-        />
-      </body>
-    </html>
+    <>
+      <Hero />
+      <Features />
+      <AboutUs />
+      <Testimonials />
+      <Newsletter />
+      <Cookies />
+      <Rdvicon />
+      <Analytics />
+      <SpeedInsights />
+    </>
   );
 }
