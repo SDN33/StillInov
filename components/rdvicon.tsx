29df0fb7b4b474c2fef139101788f4b55184f56f
SDN33 <s.deinegri2@gmail.com>
2024-09-19 11:35:36 +0200

    si

--- components/rdvicon.tsx
+++ components/rdvicon.tsx
@@ -86,7 +86,7 @@ const RdvIcon: React.FC = () => {
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
         onClick={handleTogglePopup}
-        className={`bounce-icon fixed bottom-4 right-4 md:bottom-6 md:right-6 p-3 rounded-full shadow-lg text-white bg-gradient-to-br from-orange-400 via-red-500 to-teal-400 flex items-center justify-center hover:scale-150 transition-transform duration-300 ${isPopupOpen ? 'z-50' : 'z-30'}`}
+        className={`bounce-icon fixed bottom-4 right-4 md:bottom-6 md:right-6 p-3 rounded-full shadow-lg text-white bg-gradient-to-br from-orange-400 via-red-500 to-teal-400 flex items-center justify-center hover:scale-150 transition-transform duration-300 ${isPopupOpen ? 'z-40' : 'z-30'}`}
         aria-label="Contactez-nous"
       >
         <span className="text-4xl font-bold">Si</span>
