8dd872f62ff1d1e10e4e7125fc33114be4c8e72c
SDN33 <s.deinegri2@gmail.com>
2024-09-21 17:09:52 +0200

    dl

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -43,6 +43,7 @@ export default function Testimonials() {
             autoplay={{ delay: 3500 }} // Défilement toutes les 2,5 secondes
             modules={[EffectCoverflow, Pagination, Navigation, Autoplay]} // Activer les modules ici
             className="mySwiper -z-10" // Appliquer le z-index le plus bas
+            z-index={-10} // Appliquer le z-index le plus bas
           >
             {/* 1st testimonial */}
             <SwiperSlide>
